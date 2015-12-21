127a7609b7699c9cc6cce62e6038934a7339f348
cc32054 <cc32054@NOLPWS15638519.oneadr.net>
2015-12-21 10:21:12 +0100

    first change in local

---
+++ HelloWorld/app/controllers/Application.java
@@ -0,0 +1,29 @@
+package controllers;
+
+import play.*;
+import play.data.validation.Email;
+import play.data.validation.Required;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+//Hello
+public class Application extends Controller {
+
+    public static void index() {
+        render();
+    }
+
+    public static void sayHello(@Required String fName,@Required String lName,@Email String email,@Required String pswd){
+    		render(fName,lName,email,pswd);
+    }
+    
+    public static void ticket() {
+        render();
+    } 
+    
+    public static void Detail() {
+        render();
+    } 
+}
